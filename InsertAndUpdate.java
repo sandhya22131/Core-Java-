720b9b146d850ee1d8388c1c1d76e282f882abd7
sandhya22131 <124561910+sandhya22131@users.noreply.github.com>
2025-05-21 16:11:30 +0530

    Create InsertAndUpdate.java

---
+++ InsertAndUpdate.java
@@ -0,0 +1,23 @@
+
+class StudentDAO {
+    Connection conn;
+    public StudentDAO() throws Exception {
+        conn = DriverManager.getConnection("jdbc:sqlite:students.db");
+    }
+
+    public void insertStudent(int id, String name) throws SQLException {
+        String sql = "INSERT INTO students(id, name) VALUES (?, ?)";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setInt(1, id);
+        pstmt.setString(2, name);
+        pstmt.executeUpdate();
+    }
+
+    public void updateStudent(int id, String name) throws SQLException {
+        String sql = "UPDATE students SET name = ? WHERE id = ?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, name);
+        pstmt.setInt(2, id);
+        pstmt.executeUpdate();
+    }
+}
