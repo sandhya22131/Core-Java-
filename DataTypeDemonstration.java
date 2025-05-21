e790d217777077fa46c179f024aaeb0b8cbfb360
sandhya22131 <124561910+sandhya22131@users.noreply.github.com>
2025-05-21 15:50:32 +0530

    Create DataTypeDemonstration.java

---
+++ DataTypeDemonstration.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int a=19234;
+        float b=2.567f;
+        double c=23.0000567;
+        char d='a';
+        boolean e =true;
+        
+        
+        System.out.println("The Integer value is: "+a);
+         System.out.println("The Float value is: "+b);
+          System.out.println("The Double value is: "+c);
+           System.out.println("The Character value is: "+d);
+            System.out.println("The Boolean value is: "+e);
+    }
+}
