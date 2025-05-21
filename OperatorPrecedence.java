288405f1e2bd00a371131b65247af0eb62c08e6d
sandhya22131 <124561910+sandhya22131@users.noreply.github.com>
2025-05-21 15:51:25 +0530

    Create OperatorPrecedence.java

---
+++ OperatorPrecedence.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int a,b,c,d,e;
+        
+        a=5;b=2;d=6;c=1;e=5;
+        
+        int x=a*b%c+e/d-a;
+        System.out.println("The result of the given expression is:"+x);
+        
+    }
+}
