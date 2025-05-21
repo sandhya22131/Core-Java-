06c5a626c1cd3ae2b2728e821ba51057923648da
sandhya22131 <124561910+sandhya22131@users.noreply.github.com>
2025-05-21 15:51:06 +0530

    Create TypeCasting.java

---
+++ TypeCasting.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int a=90;
+        double s=2.435;
+        
+        double sa=(double)a;
+        int as=(int)s;
+        
+        System.out.println("The original integer value is: "+a);
+        System.out.println("The original double value is: "+s);
+        
+        System.out.println("The modified double value is: "+sa);
+        System.out.println("The modified integer value is: "+as);
+    }
+}
